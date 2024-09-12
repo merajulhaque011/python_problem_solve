acdf8fb946aaeef7992deada0d12b47df1a62625
Merajul Haque <merajulhaque1712@gmail.com>
2024-09-12 21:45:04 +0600

    simple calculator.py

---
+++ simple calculator.py
@@ -0,0 +1,20 @@
+print("1:sum 2:Sub 3:Mult 4:Div")
+choice = input("Enter your choice (1/2/3/4): ")
+
+if choice in ['1', '2', '3', '4']:
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+
+        if choice == '1':
+            print(f"{"sum"} = {num1 + num2}")
+        elif choice == '2':
+            print(f"{"Sub"} = {num1 - num2}")
+        elif choice == '3':
+            print(f"{"Mult"} = {num1 * num2}")
+        elif choice == '4':
+            if num2 != 0:
+                print(f"{"Div"} = {num1 / num2}")
+            else:
+                print("Error: Division by zero is not allowed.")
+else:
+        print("Invalid input, please select a valid operation (1/2/3/4).")
